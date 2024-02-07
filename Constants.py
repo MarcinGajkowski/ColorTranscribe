c0bc57c78a3be16ed92aeb3bfc30e7f51c9f36b8
MarcinGajkowski <91730962+MarcinGajkowski@users.noreply.github.com>
2024-02-07 14:12:05 +0100

    I guess add Constants?

--- Constants.py
+++ Constants.py
@@ -2,21 +2,30 @@
 ASS_AI_API_KEY = "d722ee39e3484b3e807a1d7d024f7501"
 
 # video URLs
-polyglot12 = 'https://www.youtube.com/watch?v=Nfu30AbwNMA'
 polyglotItaliano = 'https://www.youtube.com/watch?v=9gUaWgktKQ0'
 meetTheMan = 'https://www.youtube.com/watch?v=Qac_X0J-L6I'
+tomSample = 'https://www.youtube.com/watch?v=pU9sHwNKc2c'
 
 # .wav file paths
 SAMPLE_URL1 = './ColorTransSamples/polyglotSample.wav'
 SAMPLE_URL2 = './ColorTransSamples/ItalianPolyglot.wav'
-SAMPLE_URL3 = './ColorTransSamples/someOtherGuy.wav'
+SAMPLE_TOM1 = "./ColorTransSamples/Why don't subtitles match dubbing？ [English].wav"
+SAMPLE_TOM2 = "./ColorTransSamples/Why don't subtitles match dubbing？ [French].wav"
+SAMPLE_TOM3 = "./ColorTransSamples/Why don't subtitles match dubbing？ [PT-BR].wav"
 
-# .flac file paths (with a sample rate of 16000 Hz)
+
+# .flac file paths (with a sampling rate of 16000 Hz)
 FLAC_URL1 = './ColorTransSamples/polyglotSample1.flac'
 FLAC_URL2 = './ColorTransSamples/ItalianPolyglot1.flac'
-FLAC_URL3 = './ColorTransSamples/someOtherGuy1.flac'
+FLAC_TOM1 = "./ColorTransSamples/Why don't subtitles match dubbing？ [English].flac"
+FLAC_TOM2 = "./ColorTransSamples/Why don't subtitles match dubbing？ [French].flac"
+FLAC_TOM3 = "./ColorTransSamples/Why don't subtitles match dubbing？ [PT-BR].flac"
 
-# Google URIs
+
+# Google URIs (with a sampling rate of 44100 Hz)
 GOOG_URI1 = "gs://wavcolorsbucket/polyglotSample.flac"
 GOOG_URI2 = "gs://wavcolorsbucket/ItalianPolyglot.flac"
-GOOG_URI3 = "gs://wavcolorsbucket/someOtherGuy.flac"
+GOOG_TOM1 = "gs://wavcolorsbucket/Why don't subtitles match dubbing？ [English#44100Hz].flac"
+GOOG_TOM2 = "gs://wavcolorsbucket/Why don't subtitles match dubbing？ [French#44100Hz].flac"
+GOOG_TOM3 = "gs://wavcolorsbucket/Why don't subtitles match dubbing？ [PT-BR#44100Hz].flac"
+
